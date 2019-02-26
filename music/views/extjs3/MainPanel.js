eac7bc0c3251e585360d60a95f5cb39bc894f84d
Merijn Schering <mschering@intermesh.nl>
2019-02-26 15:37:49 +0100

    Changed MainPanel

--- music/views/extjs3/MainPanel.js
+++ music/views/extjs3/MainPanel.js
@@ -1,3 +1,28 @@
 go.modules.tutorial.music.MainPanel = Ext.extend(go.modules.ModulePanel, {
-	html: "Hello world"
+
+	// Will make a single item fit in this panel. We'll change this later.
+	layout: "fit",
+
+	initComponent: function () {
+
+		//create the genre filter component
+		this.genreFilter = new go.modules.tutorial.music.GenreFilter({
+			tbar: [{
+					xtype: "tbtitle",
+					text: t("Genres")
+				}]
+		});
+
+		//add it to the main panel's items.
+		this.items = [this.genreFilter];
+
+		go.modules.tutorial.music.MainPanel.superclass.initComponent.call(this);
+
+		this.on("afterrender", function () {
+
+			//when this panel renders, load the filter.
+			this.genreFilter.store.load();
+
+		}, this);
+	}
 });
