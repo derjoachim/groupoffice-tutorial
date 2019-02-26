0d5647f4620e2af60480362f5f60f0787d4ac37c
Merijn Schering <mschering@intermesh.nl>
2019-02-26 15:45:37 +0100

    Added GenreCombo.js

---
+++ music/views/extjs3/GenreCombo.js
@@ -0,0 +1,21 @@
+go.modules.tutorial.music.GenreCombo = Ext.extend(go.form.ComboBox, {
+        fieldLabel: t("Genre"),
+        hiddenName: 'genreId',
+        anchor: '100%',
+        emptyText: t("Please select..."),
+        pageSize: 50,
+        valueField: 'id',
+        displayField: 'name',
+        triggerAction: 'all',
+        editable: true,
+        selectOnFocus: true,
+        forceSelection: true,
+        allowBlank: false,
+        store: new go.data.Store({
+                fields: ['id', 'name'],
+                entityStore: "Genre"
+        })
+});
+
+// Register an xtype so we can use the component easily.
+Ext.reg("genrecombo", go.modules.tutorial.music.GenreCombo);
