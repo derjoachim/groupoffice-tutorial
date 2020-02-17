ffdcbcd8a182263c54f17b38501dfc89a57ab4b2
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2020-02-17 12:09:37 +0100

    Code review: updated reviews model to auto-update Artist entity upos savir or deleting

--- music/model/Review.php
+++ music/model/Review.php
@@ -3,6 +3,7 @@
 
 namespace go\modules\tutorial\music\model;
 
+use Exception;
 use go\core\acl\model\AclOwnerEntity;
 use go\core\orm\Query;
 
@@ -33,6 +34,51 @@ class Review extends AclOwnerEntity
 			->addTable('music_review')
 			->setQuery((new Query())->select('a.name AS albumtitle')
 				->join('music_album', 'a','a.id=music_review.albumId'));
+	}
+
+	protected function internalSave()
+	{
+		if($this->isNew()) {
+			$this->albumtitle = go()->getDbConnection()
+				->selectSingleValue('name')
+				->from('music_album')
+				->where(['id' => $this->albumId])
+				->single();
+		}
+
+		$this->changeArtist([$this->albumId]);
+
+		return parent::internalSave();
+	}
+
+
+	protected static function internalDelete(Query $query)
+	{
+		//Create clone to avoid changes to the  original delete query object
+		$deleteQuery = clone $query;
+
+		//Select albums of artists affected by this delete
+		$deleteQuery->selectSingleValue('albumId');
+
+		static::changeArtist($deleteQuery->all());
+
+		return parent::internalDelete($query);
+	}
+
+	/**
+	 * Our review has effect on Artist entities because they implement getAlbumCount().
+	 *
+	 * @param array $albumIds
+	 * @throws Exception
+	 */
+	private static function changeArtist(array $albumIds) {
+		Artist::entityType()->changes(
+			go()->getDbConnection()
+				->select('art.id, null, 0')
+				->from('music_artist', 'art')
+				->join('music_album', 'alb', 'alb.artistId = art.id')
+				->where('alb.id', 'IN', $albumIds)
+		);
 	}
 
 	protected static function defineFilters()
